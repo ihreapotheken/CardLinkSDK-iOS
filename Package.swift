6655c603e69fc5445cbb59b68d933a067801acbe
Danijel Huis <dhuis@4ofthem.eu>
2025-01-23 10:06:19 +0100

    Cleaning

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CardLinkSDK",
-            url: "https://github.com/ihreapotheken/CardLinkSDK-iOS/releases/download/1.3.0/CardLinkSDK-1.3.0.xcframework.zip",
+            url: "https://github.com/ihreapotheken/CardLinkSDK-iOS/releases/download/xxx/CardLinkSDK-1.3.0.xcframework.zip",
             checksum: "a6d6446bbc83e416ca4182a00d93bdba4eefd415a84527c421d648c7dbd2825e"
         ),
         .target(
