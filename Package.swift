366091745c629a4f46c339d20aaef9a5709fcc7e
Danijel Huis <danijel.huis.app@gmail.com>
2025-02-27 18:31:51 +0100

    Fastlane IACardLink Release 1.5.0

--- Package.swift
+++ Package.swift
@@ -4,31 +4,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "CardLinkSDK",
+    name: "IACardLink",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "CardLinkSDK",
-            targets: ["CardLinkSDKWrapper"])
+            name: "IACardLink",
+            targets: ["IACardLinkWrapper"])
     ],
     dependencies: [
         .package(url: "https://github.com/gematik/OpenSSL-Swift", from: "4.0.0"),
     ],
     targets: [
         .binaryTarget(
-            name: "CardLinkSDK",
-            url: "https://github.com/ihreapotheken/CardLinkSDK-iOS/releases/download/1.4.5/CardLinkSDK-1.4.5.xcframework.zip",
-            checksum: "1b8a2b0194b2f7a711a02b23ef813408bb0f3b2459d257fed8e9c63a693761b7"
+            name: "IACore",
+            url: "https://github.com/ihreapotheken/CardLinkSDK-iOS/releases/download/1.5.0/IACore-1.5.0.xcframework.zip",
+            checksum: "5c0ffa3885f6f2c19814d5e0063dfee25d6ec42e02a5fac8762fb752dc7c5569"
+        ),
+        .binaryTarget(
+            name: "IACardLink",
+            url: "https://github.com/ihreapotheken/CardLinkSDK-iOS/releases/download/1.5.0/IACardLink-1.5.0.xcframework.zip",
+            checksum: "2df2de42e7e5a8f86a7fdfd935db5c6d653f000073ccbda8acac0a6c9dff6cda"
         ),
         .target(
-            name: "CardLinkSDKWrapper",
+            name: "IACardLinkWrapper",
             dependencies: [ 
-                .target(name: "CardLinkSDK"),
+                .target(name: "IACore"),
+                .target(name: "IACardLink"),
                 .product(name: "OpenSSL-Swift", package: "OpenSSL-Swift"),
             ],
-            path: "Sources/CardLinkSDKWrapper"
+            path: "Sources/IACardLinkWrapper"
         )
     ]
 )
